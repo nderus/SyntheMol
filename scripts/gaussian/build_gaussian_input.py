60e7fc5d0409f1cbf6888e85e01b2fc1710258e3
kcallon <kcallon@stanford.edu>
2024-01-27 13:17:40 -0800

    changes to max cycle for gaussian build

--- scripts/gaussian/build_gaussian_input.py
+++ scripts/gaussian/build_gaussian_input.py
@@ -7,7 +7,7 @@ from tap import tapify
 def smiles_to_coords(smiles, cluster):
     #f = open(f'./{cluster}_top_new.com', 'w')
 
-    print('#p b3lyp/3-21G* opt=(calcfc,ts,noeigen) scrf=(solvent=water) td=(singlets,nstates=5)\n')
+    print('#p b3lyp/3-21G* opt=(calcfc,ts,noeigen) scrf=(solvent=water) td=(singlets,nstates=5) scf=maxcycle=1000\n')
     print(f' {cluster}_top\n')
     mol = Chem.MolFromSmiles(smiles)
     mol = Chem.AddHs(mol)
