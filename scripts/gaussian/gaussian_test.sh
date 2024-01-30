fb80006ee76de072dfa2d132b42a92fc35cc0e63
Jennifer Jing Xu <jennxu23@sh02-ln04.stanford.edu>
2024-01-29 17:53:51 -0800

    modified shell script

--- scripts/gaussian/gaussian_test.sh
+++ scripts/gaussian/gaussian_test.sh
@@ -13,20 +13,21 @@
 #SBATCH --error=error/synthemol/%a.err
 #SBATCH --time=48:00:00
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=kcallon@stanford.edu
+#SBATCH --mail-user=jennxu23@stanford.edu
 
 
-source /home/users/kcallon/.bashrc
-conda activate synthemol
+source /home/users/jennxu23/.bashrc
+ml load python/3.9.0
 ml load chemistry gaussian
-cd /scratch/users/kcallon/SyntheMol_private/scripts/gaussian
+ml load py-pandas/2.0.1_py39
+cd /home/users/jennxu23/synthemol/SyntheMol_private/scripts/gaussian
 
 # Capture the input and output files
 input_file="sumita.txt"
 output_file="sumita_out.log"
 
 # Check if the SMILES and properties file exists
-if [ ! -f $input_file ]; then
+if [ ! -f "$input_file" ]; then
     echo "Error: SMILES and properties file ${input_file}  not found."
     exit 1
 fi
@@ -34,12 +35,13 @@ fi
    
    
 while IFS=',', read -r cluster smiles; do
-   python3 build_gaussian_input.py --smiles ${smiles} --cluster ${cluster} > generated_file.com
+   python3 build_gaussian_input.py --smiles "${smiles}" --cluster "${cluster}" > generated_file.com
    
    if [ $? -eq 0 ]; then
       echo "starting gaussian"
-      g16 < generated_file.com > generated_log.log
+      g16 < generated_file.com
+      #python3 calc_homolumo.py --inputfilename generated_log.log --smiles_str "$smiles"> "$output_file"
    fi
-done < $input_file
+done < "$input_file"
 
 echo "Processing complete."
