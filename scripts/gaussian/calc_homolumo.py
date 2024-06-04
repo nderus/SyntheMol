1d6db360868699bfe82a781b3cd0e5c74aa7ec61
jennxu23 <jennxu23@stanford.edu>
2024-06-04 13:17:20 -0700

    modified calc_homolumo to get oscillator strength and dipole moment

--- scripts/gaussian/calc_homolumo.py
+++ scripts/gaussian/calc_homolumo.py
@@ -1,9 +1,13 @@
+
 from tap import tapify
 
 def extract_energies(lines):
     alphaGaps = []
     excited_state_energies = []
-    
+    oscillation = []
+    dipoles = []
+    prev_line = ""
+
     for line in lines:
         if line.find("Alpha virt. eigenvalues --") >=0:
             if prev_line.find("Alpha  occ. eigenvalues --") >= 0:
@@ -13,21 +17,34 @@ def extract_energies(lines):
                 prev_line_StateInfo = prev_line_removed.split()
                 alphaGap = 1240/abs(27.211*float(prev_line_StateInfo[-1]) - float(line_StateInfo[0]))
                 alphaGaps.append(alphaGap)
-        prev_line = line
+        
 
         if ' Excited State   1:' in line:
             parts = line.split()
+            oscillation.append(float(parts[8][2:]))
             excited_state_energies.append(float(parts[6]))
 
+        if 'Dipole moment (field-independent basis, Debye):' in prev_line:
+            parts = line.split()
+            dipoles.append(float(parts[-1]))
+        prev_line = line
 
-    return excited_state_energies, alphaGaps
+    return excited_state_energies, oscillation, dipoles, alphaGaps
 
  
 def main(inputfilename, smiles_str):
     f = open(inputfilename, 'r')
     lines = f.readlines()
-    excited_state_energies, alphaGaps = extract_energies(lines)
-    print(smiles_str, excited_state_energies, alphaGaps)
+    excited_state_energies, oscillation, dipoles, alphaGaps = extract_energies(lines)
+    print(smiles_str),
+    print()
+    print('Excited State Energies:', excited_state_energies)
+    print()
+    print('Oscillator Strengths:', oscillation)
+    print()
+    print('Dipole moments:', dipoles)
+    print()
+    print('Alpha gaps:', alphaGaps)
 
 if __name__ == "__main__":
     tapify(main)
---
+++ scripts/gaussian/get_gaussian_values.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the path to the file containing the list of files
+FILE_LIST="file_list.csv"
+
+# Define the path to the Python script
+PYTHON_SCRIPT="calc_homolumo.py"
+
+# Iterate over each line in the file list
+while IFS=',' read -r string file; do
+    # Generate the output filename
+    file=$(echo "$file" | tr -d '\r')
+    output_file="${file/_log.log/_out.txt}"
+    # Run the Python script on the current file and direct output to the output file
+    python3 "$PYTHON_SCRIPT" --inputfilename "$file" --smiles_str "$string" > "$output_file"
+done < "$FILE_LIST"
