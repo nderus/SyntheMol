bda53deb0ce0fa8baa88414c03ba7ffe0c9a790c
kcallon <kcallon@stanford.edu>
2026-01-04 19:45:10 -0800

    add sp2_net to script

--- synthemol/resources/combine_building_blocks.py
+++ synthemol/resources/combine_building_blocks.py
@@ -3,14 +3,7 @@ import pandas as pd
 building_real_df = pd.read_csv('building_blocks_plqy_score_real_class.csv')
 building_emi_df = pd.read_csv('building_blocks_emi_score_real.csv')
 building_ab_df = pd.read_csv('building_blocks_abs_score_real.csv')
-
-smiles_equal = (building_real_df['smiles'] == building_emi_df['smiles']).all() and (building_real_df['smiles'] == building_ab_df['smiles']).all()
-size_equal = len(building_real_df) == len(building_emi_df) == len(building_ab_df)
-
-if smiles_equal and size_equal:
-    print("The 'smiles' column is in the same order and size in all three DataFrames.")
-else:
-    print("The 'smiles' column is not in the same order and size in all three DataFrames.")
+building_sp2_df = pd.read_csv('building_blocks_sp2_net_real.csv')
 
 # Change the column titled 'binary' to 'PLQY' in one of the DataFrames
 building_real_df.rename(columns={'binary': 'PLQY'}, inplace=True)
@@ -20,6 +13,9 @@ building_real_df['Emission/nm'] = building_emi_df['Emission/nm']
 
 # Append 'ab' column from building_ab_df to building_real_df
 building_real_df['Absorption/nm'] = building_ab_df['Absorption/nm']
+
+# Add sp2_net from building_sp2_df to building_real_df
+building_real_df['sp2_net'] = building_sp2_df['sp2_net']
 
 # Save the updated DataFrame back to a CSV file
 building_real_df.to_csv('combined_building_blocks_score_class_real_0.5.csv', index=False)
